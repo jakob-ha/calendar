231e051ed7c127cf3fc551c3031ed4f8f17d7dd1
jakobmakela <156580100+jakob-ha@users.noreply.github.com>
2026-04-25 17:12:46 +0300

    event mini more prepared to display data

--- lib/components/event_mini.dart
+++ lib/components/event_mini.dart
@@ -6,6 +6,28 @@ class EventMini  extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Card(
+      // clipBehavior is necessary because, without it, the InkWell's animation
+      // will extend beyond the rounded edges of the [Card] (see https://github.com/flutter/flutter/issues/109776)
+      // This comes with a small performance cost, and you should not set [clipBehavior]
+      // unless you need it.
+        clipBehavior: Clip.hardEdge,
+        child: InkWell(
+          splashColor: Colors.blue.withAlpha(30),
+          onTap: () {
+            debugPrint('Card tapped.');
+          },
+          child: const SizedBox(
+            width: null,
+            height: null,
+            child: Column(
+              children: [
+                Text('data'),
+                Text('data')
+              ],
+            ),
+          ),
+        ),
+    );
   }
 }
