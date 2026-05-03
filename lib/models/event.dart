42bc7c479234d5937093b4525942498a2e45652e
jakobmakela <156580100+jakob-ha@users.noreply.github.com>
2026-05-03 16:11:48 +0300

    to string of event

--- lib/models/event.dart
+++ lib/models/event.dart
@@ -1,3 +1,6 @@
+
+import 'package:intl/intl.dart';
+
 class Event {
   final String name;
   final DateTime start;
@@ -8,4 +11,7 @@ class Event {
     required this.start,
     required this.end,
   });
+
+  @override
+  String toString() => '$name from ${DateFormat('HH:mm').format(start)} to ${DateFormat('HH:mm').format(end)}';
 }
